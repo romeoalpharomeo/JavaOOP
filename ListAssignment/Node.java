3cec564f72cf590099ecd886cf5e499e530b6aee
romeoalpharomeo <ryanrey0333@gmail.com>
2021-06-09 16:08:36 -0400

    attempt sll

---
+++ ListAssignment/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    public int value;
+    public Node next;
+    // This is the node setter
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
